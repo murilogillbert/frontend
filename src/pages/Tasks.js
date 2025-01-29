19159fef1f5b2728451e821f3e34b1a4417cf1a7
murilogillbert <murilo.gillbert@outlook.com>
2025-01-28 23:01:20 -0400

    Rota atualizada

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -24,7 +24,7 @@ const Tasks = () => {
   // Função para buscar as tarefas do backend
   const fetchTasks = async (token) => {
     try {
-      const response = await axios.get("https://valiant-nurturing.up.railway.app/todos", {
+      const response = await axios.get("backend-todo-production-7edc.up.railway.app/todos", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -47,7 +47,7 @@ const Tasks = () => {
 
     try {
       const response = await axios.post(
-        "https://valiant-nurturing.up.railway.app/todos",
+        "backend-todo-production-7edc.up.railway.app/todos",
         { title: newTask },
         {
           headers: {
@@ -69,7 +69,7 @@ const Tasks = () => {
     const token = localStorage.getItem("accessToken");
 
     try {
-      await axios.delete(`https://valiant-nurturing.up.railway.app/todos/${taskId}`, {
+      await axios.delete(`backend-todo-production-7edc.up.railway.app/todos/${taskId}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
